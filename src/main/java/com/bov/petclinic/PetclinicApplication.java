0ae1e54d9e004c1fa4da65742d64f820c5d32332
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-03-26 19:26:41 +0200

    Initial commit.

---
+++ src/main/java/com/bov/petclinic/PetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.bov.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicApplication.class, args);
+    }
+
+}
