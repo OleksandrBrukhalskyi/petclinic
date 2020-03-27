0da98cef1f5569ff334889e88191f2a50134e468
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-03-27 14:57:37 +0200

    feat: add annotaions to the fields.

--- src/main/java/com/bov/petclinic/Pet.java
+++ src/main/java/com/bov/petclinic/Pet.java
@@ -2,15 +2,21 @@ package com.bov.petclinic;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity(name = "pets")
 @Data
 public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "date_of_birth")
     private Date dateOfBirth;
+    @Column(name = "type")
     private String type;
+    @ManyToOne
     private Owner owner;
 }
