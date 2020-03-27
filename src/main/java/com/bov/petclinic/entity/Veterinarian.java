38f48bdd495ed69bca82891736ea185326aef8c5
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-03-27 15:15:36 +0200

    feat: create Veterinarian class.

--- src/main/java/com/bov/petclinic/entity/Veterinarian.java
+++ src/main/java/com/bov/petclinic/entity/Veterinarian.java
@@ -1,4 +1,22 @@
 package com.bov.petclinic.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity(name = "veterinarians")
+@Data
 public class Veterinarian {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "firstname")
+    private String firstname;
+    @Column(name = "patronymic")
+    private String patronymic;
+    @OneToOne
+    private Specialty specialty;
+
 }
