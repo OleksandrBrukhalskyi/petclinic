e0321195a5e210fa2c3b69b8fe0edd46cfc0a65d
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-03-27 14:46:14 +0200

    feat: add fields and annotations in Pet class.

--- src/main/java/com/bov/petclinic/Pet.java
+++ src/main/java/com/bov/petclinic/Pet.java
@@ -1,4 +1,16 @@
 package com.bov.petclinic;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+@Entity(name = "pets")
+@Data
 public class Pet {
+    private Long id;
+    private String name;
+    private Date dateOfBirth;
+    private String type;
+    private Owner owner;
 }
