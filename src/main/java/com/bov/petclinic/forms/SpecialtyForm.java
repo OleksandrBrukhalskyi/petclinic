74a1e19970a9aaed8e5b82a8ad472afdd1d74dd3
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-03-28 19:59:03 +0200

    feat: create SpecialtyForm.

--- src/main/java/com/bov/petclinic/forms/SpecialtyForm.java
+++ src/main/java/com/bov/petclinic/forms/SpecialtyForm.java
@@ -1,4 +1,11 @@
 package com.bov.petclinic.forms;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SpecialtyForm {
+    private Long id;
+    private String specialty;
 }
