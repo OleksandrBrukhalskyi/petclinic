9b0bc285e74e04b45bdfadc2f7e8e88b5a78e389
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-03-28 20:00:23 +0200

    feat: create VeterinarianForm.

--- src/main/java/com/bov/petclinic/forms/VeterinarianForm.java
+++ src/main/java/com/bov/petclinic/forms/VeterinarianForm.java
@@ -1,4 +1,15 @@
 package com.bov.petclinic.forms;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class VeterinarianForm {
+
+    private Long id;
+    private String surname;
+    private String firstname;
+    private String patronymic;
+    private Long specialty;
 }
