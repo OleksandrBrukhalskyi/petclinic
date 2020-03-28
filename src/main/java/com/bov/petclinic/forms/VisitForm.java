004b74fd7cb350b6edca036997b131f6c71daddb
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-03-28 20:01:37 +0200

    feat: create VisitForm.

--- src/main/java/com/bov/petclinic/forms/VisitForm.java
+++ src/main/java/com/bov/petclinic/forms/VisitForm.java
@@ -1,4 +1,12 @@
 package com.bov.petclinic.forms;
 
+import java.util.Date;
+
 public class VisitForm {
+
+    private Long id;
+    private Date visitDate;
+    private String goalOfVisit;
+    private Long pet;
+    private Long owner;
 }
