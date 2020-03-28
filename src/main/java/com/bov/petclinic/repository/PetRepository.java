d1bd99053dd5ae3b594e2b016ff3e3e13588d0c5
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-03-28 20:11:55 +0200

    feat: add @Repository annotation in PetRepository.

--- src/main/java/com/bov/petclinic/repository/PetRepository.java
+++ src/main/java/com/bov/petclinic/repository/PetRepository.java
@@ -2,6 +2,8 @@ package com.bov.petclinic.repository;
 
 import com.bov.petclinic.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PetRepository extends JpaRepository<Pet,Long> {
 }
