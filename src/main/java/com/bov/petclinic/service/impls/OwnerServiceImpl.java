0a2aa8e51fafa76bc055ed374f1ac501d13abc38
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-04-16 13:47:04 +0300

    feat: add logic to OwnerService methods. And add logging for service.

--- src/main/java/com/bov/petclinic/service/impls/OwnerServiceImpl.java
+++ src/main/java/com/bov/petclinic/service/impls/OwnerServiceImpl.java
@@ -3,37 +3,44 @@ package com.bov.petclinic.service.impls;
 import com.bov.petclinic.entity.Owner;
 import com.bov.petclinic.repository.OwnerRepository;
 import com.bov.petclinic.service.OwnerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class OwnerServiceImpl implements OwnerService {
     @Autowired
     private OwnerRepository ownerRepository;
     @Override
     public Owner create(Owner owner) {
-        return null;
+        log.info("Owner created");
+        return ownerRepository.save(owner);
     }
 
     @Override
     public Owner update(Owner owner) {
-        return null;
+        log.info("Owner updated");
+        return ownerRepository.save(owner);
     }
 
     @Override
     public Owner getById(Long id) {
-        return null;
+        log.info("Owner found");
+        return ownerRepository.findById(id).orElseThrow(() -> new RuntimeException("Owner did not found"));
     }
 
     @Override
     public void delete(Long id) {
-
+        log.info("Onwer deleted");
+        this.ownerRepository.deleteById(id);
     }
 
     @Override
     public List<Owner> getAll() {
+        log.info("List of Owners");
         return ownerRepository.findAll();
     }
 }
