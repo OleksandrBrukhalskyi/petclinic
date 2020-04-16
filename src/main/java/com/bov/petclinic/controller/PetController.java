0509ee0e5e0281cac21d53812a0c3414e83758bc
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-04-16 18:33:41 +0300

    feat: create PetController.

---
+++ src/main/java/com/bov/petclinic/controller/PetController.java
@@ -0,0 +1,66 @@
+package com.bov.petclinic.controller;
+
+import com.bov.petclinic.entity.Owner;
+import com.bov.petclinic.entity.Pet;
+import com.bov.petclinic.forms.PetForm;
+import com.bov.petclinic.repository.OwnerRepository;
+import com.bov.petclinic.service.OwnerService;
+import com.bov.petclinic.service.PetService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/pet")
+public class PetController {
+
+    @Autowired
+    private PetService petService;
+    @Autowired
+    private OwnerService ownerService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Pet> create(@Valid @RequestBody PetForm petForm){
+        Owner owner = ownerService.getById(petForm.getId());
+        Pet pet = new Pet();
+        pet.setName(petForm.getName());
+        pet.setBreed(petForm.getBreed());
+        pet.setDateOfBirth(petForm.getDateOfBirth());
+        pet.setOwner(owner);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(petService.create(pet));
+
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Pet> update(@Valid @RequestBody PetForm petForm ,@PathVariable("id") Long id){
+        Owner owner = ownerService.getById(petForm.getId());
+        Pet pet = new Pet();
+        pet.setName(petForm.getName());
+        pet.setBreed(petForm.getBreed());
+        pet.setDateOfBirth(petForm.getDateOfBirth());
+        pet.setOwner(owner);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(petService.update(pet));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Pet> getById(@PathVariable("id") Long id){
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .body(petService.getById(id));
+    }
+    @GetMapping
+    public List<Pet> getAll(){
+        return petService.getAll();
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id){
+        this.petService.delete(id);
+    }
+}
