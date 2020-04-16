709523fdd6a74b58dc5e3dad2375410e7ef2b8b8
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-04-16 18:45:03 +0300

    feat: add logic to PetService methods. And add logging for service.

--- src/main/java/com/bov/petclinic/service/impls/PetServiceImpl.java
+++ src/main/java/com/bov/petclinic/service/impls/PetServiceImpl.java
@@ -1,35 +1,47 @@
 package com.bov.petclinic.service.impls;
 
 import com.bov.petclinic.entity.Pet;
+import com.bov.petclinic.repository.PetRepository;
 import com.bov.petclinic.service.PetService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class PetServiceImpl implements PetService {
+    @Autowired
+    private PetRepository petRepository;
+
     @Override
     public Pet create(Pet pet) {
-        return null;
+        log.info("Pet added",pet);
+        return petRepository.save(pet);
     }
 
     @Override
     public Pet update(Pet pet) {
-        return null;
+        log.info("Pet updated!");
+        return petRepository.save(pet);
     }
 
     @Override
     public Pet getById(Long id) {
-        return null;
+        log.info("Pet found by this id:",id);
+        return petRepository.findById(id).orElseThrow(() -> new RuntimeException("Pet not found"));
     }
 
     @Override
     public void delete(Long id) {
-
+        log.info("Pet deleted");
+        this.petRepository.deleteById(id);
     }
 
     @Override
     public List<Pet> getAll() {
-        return null;
+        log.info("List of pets",petRepository.findAll());
+        return petRepository.findAll();
     }
 }
