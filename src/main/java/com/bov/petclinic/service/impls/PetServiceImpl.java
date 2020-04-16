2b12be9d871ec674cac4d89e28977fc2133eee18
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-04-16 13:37:21 +0300

    feat: create PetServiceImpl class.

---
+++ src/main/java/com/bov/petclinic/service/impls/PetServiceImpl.java
@@ -0,0 +1,35 @@
+package com.bov.petclinic.service.impls;
+
+import com.bov.petclinic.entity.Pet;
+import com.bov.petclinic.service.PetService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PetServiceImpl implements PetService {
+    @Override
+    public Pet create(Pet pet) {
+        return null;
+    }
+
+    @Override
+    public Pet update(Pet pet) {
+        return null;
+    }
+
+    @Override
+    public Pet getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public List<Pet> getAll() {
+        return null;
+    }
+}
