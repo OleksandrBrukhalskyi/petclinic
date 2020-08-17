460b28ffe2b0aa4113024666284d4fb7b872a8eb
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-08-17 13:23:00 +0300

    feat: create OwnerDto.

---
+++ src/main/java/com/bov/petclinic/dto/OwnerDto.java
@@ -0,0 +1,16 @@
+package com.bov.petclinic.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OwnerDto {
+    private String surname;
+    private String firstname;
+    private String patronymic;
+    private String homeAddress;
+    private String number;
+}
