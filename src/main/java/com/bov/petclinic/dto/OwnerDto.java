42c3669f7cd89d116a6fdc163964e12dc8388758
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-08-24 13:11:30 +0300

    feat: add @Builder to Dto

--- src/main/java/com/bov/petclinic/dto/OwnerDto.java
+++ src/main/java/com/bov/petclinic/dto/OwnerDto.java
@@ -1,12 +1,14 @@
 package com.bov.petclinic.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class OwnerDto {
     private Long id;
     private String surname;
