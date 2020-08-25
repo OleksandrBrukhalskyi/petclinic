6708e24859bdf649a5a1bd759041001c05adcccc
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-08-25 17:16:17 +0300

    fix code smells.

--- src/main/java/com/bov/petclinic/exceptions/BadIdException.java
+++ src/main/java/com/bov/petclinic/exceptions/BadIdException.java
@@ -1,6 +1,7 @@
 package com.bov.petclinic.exceptions;
 
-public class BadIdException {
-    public BadIdException() {
+public class BadIdException extends RuntimeException {
+    public BadIdException(String message) {
+        super(message);
     }
 }
