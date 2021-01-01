4b82c8056b9c00e26a35d6f910bda7c6aa37e5a8
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-01-01 16:28:29 +0200

    fix: resolve code smells: delete empty test method in VeterinarianService tests.

--- src/test/java/com/bov/petclinic/service/VeterinarianServiceImplTests.java
+++ src/test/java/com/bov/petclinic/service/VeterinarianServiceImplTests.java
@@ -1,15 +1,26 @@
 package com.bov.petclinic.service;
 
+import com.bov.petclinic.entity.Specialty;
+import com.bov.petclinic.entity.Veterinarian;
 import com.bov.petclinic.repository.VeterinarianRepository;
 import com.bov.petclinic.service.impls.VeterinarianServiceImpl;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
 
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.Silent.class)
@@ -20,9 +31,19 @@ public class VeterinarianServiceImplTests {
     @Mock
     private VeterinarianRepository veterinarianRepository;
 
+    private Specialty specialty = new Specialty(3L,"Хірург");
 
     @Test
-    public void getAll(){
+    public void create(){
+        Veterinarian veterinarian = new Veterinarian();
+        veterinarian.setId(1L);
+        veterinarian.setSurname("Іванов");
+        veterinarian.setFirstname("Іван");
+        veterinarian.setPatronymic("Іванович");
+        veterinarian.setSpecialty(specialty);
 
+        when(veterinarianRepository.save(veterinarian)).thenReturn(veterinarian);
+        assertEquals(veterinarian,veterinarianService.create(veterinarian));
     }
+
 }
