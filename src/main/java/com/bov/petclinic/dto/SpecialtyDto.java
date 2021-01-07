569110b5b06c0c9d4c5b6ee0040422cf059fed4a
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-01-07 14:37:53 +0200

    create SpecialtyDto

---
+++ src/main/java/com/bov/petclinic/dto/SpecialtyDto.java
@@ -0,0 +1,13 @@
+package com.bov.petclinic.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SpecialtyDto {
+    private long id;
+    private String name;
+}
