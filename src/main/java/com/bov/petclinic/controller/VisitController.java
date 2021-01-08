f4081505ff7a1d433bbf807c7d7007d1d540d9f2
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-01-08 13:31:01 +0200

    feat: create VisitController.

---
+++ src/main/java/com/bov/petclinic/controller/VisitController.java
@@ -0,0 +1,82 @@
+package com.bov.petclinic.controller;
+
+import com.bov.petclinic.constant.HttpStatuses;
+import com.bov.petclinic.dto.VisitRequestDto;
+import com.bov.petclinic.dto.VisitResponseDto;
+import com.bov.petclinic.entity.Visit;
+import com.bov.petclinic.service.VisitService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/visit")
+public class VisitController {
+    private final VisitService visitService;
+
+    @Autowired
+    public VisitController(VisitService visitService) {
+        this.visitService = visitService;
+    }
+    @ApiOperation(value = "Save visit")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = HttpStatuses.OK),
+            @ApiResponse(code = 400, message = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(code = 403, message = HttpStatuses.FORBIDDEN)
+    })
+    @PostMapping("/add")
+    public ResponseEntity<VisitResponseDto> create(@Valid @RequestBody VisitRequestDto visitRequestDto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(visitService.create(visitRequestDto));
+    }
+    @ApiOperation(value = "Update visit")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = HttpStatuses.OK),
+            @ApiResponse(code = 400, message = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(code = 403, message = HttpStatuses.FORBIDDEN)
+    })
+    @PutMapping("/{id}")
+    public ResponseEntity<VisitResponseDto> update(@Valid @RequestBody VisitRequestDto visitRequestDto, @PathVariable("id") long id){
+        visitRequestDto.setId(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(visitService.update(visitRequestDto));
+    }
+    @ApiOperation(value = "Retrieve visit by id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = HttpStatuses.OK),
+            @ApiResponse(code = 400, message = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(code = 403, message = HttpStatuses.FORBIDDEN)
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<Visit> retrieveById(@PathVariable("id") long id){
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .body(visitService.getById(id));
+    } @ApiOperation(value = "Get all visits")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = HttpStatuses.OK),
+            @ApiResponse(code = 400, message = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(code = 403, message = HttpStatuses.FORBIDDEN)
+    })
+    @GetMapping
+    public List<Visit> getAll(){
+        return visitService.getAll();
+    }
+    @ApiOperation(value = "Delete visit by id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = HttpStatuses.OK),
+            @ApiResponse(code = 400, message = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(code = 403, message = HttpStatuses.FORBIDDEN)
+    })
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") long id){
+        visitService.delete(id);
+    }
+
+}
