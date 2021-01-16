590abc2710bedd621c035ed86dfbc347d1620b6d
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-01-16 14:51:50 +0200

    feat: create UserRepository.

---
+++ src/main/java/com/bov/petclinic/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.bov.petclinic.repository;
+
+import com.bov.petclinic.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByLogin(String login);
+    User findByEmail(String email);
+}
