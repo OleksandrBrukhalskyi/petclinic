19b2dba6f31a4c0fab9d072650dbf918b65a92f9
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-01-16 14:54:21 +0200

    featL create RoleRepository.

---
+++ src/main/java/com/bov/petclinic/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.bov.petclinic.repository;
+
+import com.bov.petclinic.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Role findByName(String name);
+}
