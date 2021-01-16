234cf1ccaa0ee861c63861496eb4d648915f0f8b
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-01-16 14:44:43 +0200

    feat: create Role entity.

---
+++ src/main/java/com/bov/petclinic/entity/Role.java
@@ -0,0 +1,19 @@
+package com.bov.petclinic.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String name;
+}
