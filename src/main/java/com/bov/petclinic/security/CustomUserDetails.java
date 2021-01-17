7c3304cee5203aecbc3de884cfdeabe5c4fad19c
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-01-17 17:51:07 +0200

    feat: creare CustomUserDetails.

---
+++ src/main/java/com/bov/petclinic/security/CustomUserDetails.java
@@ -0,0 +1,65 @@
+package com.bov.petclinic.security;
+
+import com.bov.petclinic.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+    private String login;
+    private String password;
+    private Collection<? extends GrantedAuthority> grantedAuthorities;
+
+    private CustomUserDetails fromUserEntityToCustomUserDetails(User user){
+        CustomUserDetails customUserDetails = new CustomUserDetails();
+        customUserDetails.login = user.getLogin();
+        customUserDetails.password = user.getPassword();
+        customUserDetails.grantedAuthorities = Collections.singletonList(new SimpleGrantedAuthority(user.getRole().getName()));
+        return customUserDetails;
+    }
+
+
+    public String getLogin() {
+        return login;
+    }
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
