6e4b5e54880623d5ec54e24c3f6979a5a021aad3
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-01-17 18:46:15 +0200

    feat: create CustomUserDetailsService.

---
+++ src/main/java/com/bov/petclinic/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.bov.petclinic.security;
+
+import com.bov.petclinic.entity.User;
+import com.bov.petclinic.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    @Autowired
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        User user = userService.findByLogin(login);
+        return CustomUserDetails.fromUserEntityToCustomUserDetails(user);
+    }
+}
