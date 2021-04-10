90ed8d3efd10c6cdfcd7b53b470dcbaad63390eb
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-04-10 20:33:15 +0300

    feat: add two additional methods for retrieving and checking user by its email

--- src/main/java/com/bov/petclinic/repository/UserRepository.java
+++ src/main/java/com/bov/petclinic/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.bov.petclinic.repository;
 import com.bov.petclinic.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User,Long> {
     User findByLogin(String login);
     Boolean existsByLogin(String login);
+    User findByEmail(String email);
+    Boolean existsByEmail(String email);
+
 }
