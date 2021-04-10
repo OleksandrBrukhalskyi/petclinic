c7c45f2eb290a089a5335b0b8867b5d79374ca76
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-04-10 22:00:24 +0300

    feat: create MailContentBuilderSerivce for creating email template.

---
+++ src/main/java/com/bov/petclinic/service/impls/MailContentBuilderService.java
@@ -0,0 +1,18 @@
+package com.bov.petclinic.service.impls;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+@AllArgsConstructor
+public class MailContentBuilderService {
+    private final TemplateEngine templateEngine;
+
+    public String build(String message){
+        Context context = new Context();
+        context.setVariable("message",message);
+        return templateEngine.process("mailTeplate",context);
+    }
+}
