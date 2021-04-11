7b3da70fcb3f791eb3654633ed722ccae6de1000
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-04-11 13:37:31 +0300

    feat: configure AuthenticationManagerBuilder in security config.

--- src/main/java/com/bov/petclinic/config/SecurityConfig.java
+++ src/main/java/com/bov/petclinic/config/SecurityConfig.java
@@ -5,11 +5,15 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -25,6 +29,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private JwtFilter jwtFilter;
+    private final UserDetailsService userDetailsService;
     @Autowired
     public void setJwtFilter(JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
@@ -36,18 +41,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .cors()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/api/register","/api/auth").permitAll()
-                .antMatchers("/api/veterinarian").hasAuthority("USER")
-                .antMatchers("/api/pet").hasAuthority("ADMIN")
+                .antMatchers("/api/veterinarian/").hasRole("USER")
+                .antMatchers("/api/pet/").hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
                 .logout().permitAll()
                 .and()
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Bean
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+
+
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
