98d8d3efb448b74be0aa2a7b6830377122c77dae
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-04-11 14:09:28 +0300

    feat: add additional fields into AuthResponse for showing refresh token, expiry time and login.

--- src/main/java/com/bov/petclinic/security/AuthResponse.java
+++ src/main/java/com/bov/petclinic/security/AuthResponse.java
@@ -1,10 +1,19 @@
 package com.bov.petclinic.security;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AuthResponse {
     private String token;
+    private String refreshToken;
+    private Instant expiresAt;
+    private String login;
 }
