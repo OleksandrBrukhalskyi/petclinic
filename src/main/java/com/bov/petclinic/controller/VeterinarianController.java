8db9edcf3dd41cf1fe98f392f420a28761ddd536
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-04-12 16:07:23 +0300

    feat: add cross origin for having access to resource from angular application.

--- src/main/java/com/bov/petclinic/controller/VeterinarianController.java
+++ src/main/java/com/bov/petclinic/controller/VeterinarianController.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/veterinarian")
+@CrossOrigin(origins = "*")
 public class VeterinarianController {
     private final VeterinarianService veterinarianService;
 
