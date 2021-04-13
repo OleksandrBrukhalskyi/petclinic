75daed3d7ca10460264a2414b51608cb258741e4
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-04-13 19:24:31 +0300

    feat: add cross origin's value for any ports of web applications.

--- src/main/java/com/bov/petclinic/controller/SpecialtyController.java
+++ src/main/java/com/bov/petclinic/controller/SpecialtyController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/specialty")
-@CrossOrigin("4200")
+@CrossOrigin(origins = "*")
 public class SpecialtyController {
     private final SpecialtyService specialtyService;
 
