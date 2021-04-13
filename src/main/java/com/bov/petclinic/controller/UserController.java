e04b84f6021d5faeae780697cb7d662428f37b63
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-04-13 19:25:09 +0300

    feat: add method for updating user.

--- src/main/java/com/bov/petclinic/controller/UserController.java
+++ src/main/java/com/bov/petclinic/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.bov.petclinic.controller;
 
+import com.bov.petclinic.constant.HttpStatuses;
+import com.bov.petclinic.dto.user.UserDto;
 import com.bov.petclinic.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -19,5 +24,16 @@ public class UserController {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
     }
-
+    @ApiOperation(value = "Update specialty")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = HttpStatuses.OK),
+            @ApiResponse(code = 400, message = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(code = 403, message = HttpStatuses.FORBIDDEN)
+    })
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("id") long id){
+        userDto.setId(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(userService.update(userDto));
+    }
 }
