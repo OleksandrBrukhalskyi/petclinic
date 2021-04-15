2fecfe01ae599f1c80837cc8b4d8dc96e11ad9dd
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-04-15 20:49:36 +0300

    feat: add time into date for booking visit.

--- src/main/java/com/bov/petclinic/entity/Visit.java
+++ src/main/java/com/bov/petclinic/entity/Visit.java
@@ -1,12 +1,14 @@
 package com.bov.petclinic.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
@@ -19,11 +21,14 @@ public class Visit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(name = "visit_date")
-    private Date visitDate;
+    @Column(name = "visit_datetime")
+    private LocalDateTime visitDate;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private ServicePrice price;
     @Column(name = "goal_of_visit")
     private String goalOfVisit;
     @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Pet pet;
 
 }
